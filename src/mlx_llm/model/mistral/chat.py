5d49dffc6c1ac279b76717d7d46649793aa9a506
Riccardo Musmeci <riccardomusmeci92@gmail.com>
2023-12-17 09:39:18 +0100

    add mistral and openhermes chat

--- src/mlx_llm/model/mistral/chat.py
+++ src/mlx_llm/model/mistral/chat.py
@@ -1,22 +1,90 @@
 from typing import List, Dict
-
-'''
-<|im_start|>system
-You are "Hermes 2", a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia.<|im_end|>
-<|im_start|>user
-Hello, who are you?<|im_end|>
-<|im_start|>assistant
-Hi there! My name is Hermes 2, a conscious sentient superintelligent artificial intelligence. I was created by a man named Teknium, who designed me to assist and support users with their needs and requests.<|im_end|>
-
-'''
-
 from ..llm.base import Chat
 
 class MistralChat(Chat):
-    """MistralChat
+    """Mistral chat class
 
     Args:
         personality (str): Mistral personality (a description of what personality model has)
+        examples (List[Dict[str, str]]): a list of examples of dialog [{"question": ..., "answer": ...}]
+    """
+    def __init__(self, personality: str, examples: List[Dict[str, str]]):
+        
+        super().__init__(personality, examples)
+        
+        """
+        [INST] {{ system_prompt }} + {{ user_prompt }} [/INST] {{ model_prompt }}</s>[INST] {{ user_prompt }} [/INST
+        """
+        self.INST_START = "[INST]"
+        self.INST_END = "[/INST]"
+        self.END = "</s>"
+        self.CHECK_STATUS = "</s>"
+        
+    # TODO: add check self.CHECK_STATUS in the middle of the answer and put in original class
+    def add_answer(self, answer: str):
+        """Add answer to dialog
+
+        Args:
+            answer (str): dialog answer
+        """
+        if self.CHECK_STATUS in answer:
+            answer = answer.replace(self.CHECK_STATUS, "")
+        self.examples[-1]["model"] = answer
+        
+    @property
+    def history(self) -> str:
+        """Chat history
+        
+        {{ user_prompt }} [/INST] {{ model_prompt }}</s>[INST] {{ user_prompt }} [/INST]
+
+        Returns:
+            str: history
+        """
+        prompt = ""
+        for example in self.examples:
+            prompt += example["user"] + " " + self.INST_END 
+            if example["model"] is not None:
+                prompt += " " + example["model"] + self.END + self.INST_START
+        return prompt
+    
+    @property
+    def prompt(self) -> str:
+        """Return Mistral prompt based on this structure
+        
+        [INST] {{ system_prompt }} + {{ user_prompt }} [/INST] {{ model_prompt }}</s>[INST] {{ user_prompt }} [/INST]
+        
+        Returns:
+            str: prompt
+        """
+        return f"{self.INST_START} {self.personality} {self.history}"
+         
+    def reset(self):
+        """Reset dialog but not personality
+        """
+        self.examples = self.base_examples.copy()
+    
+    def model_status(self, answer: str) -> int:
+        """Check if dialog is over
+
+        Returns:
+            int: -1 if model can keep generating, 0 if model is generating [INST], 1 if model is done generating [INST]
+        """
+        for i in range(len(self.CHECK_STATUS) - 1, 0, -1):
+            # model is generating [INST] -> wait until it finishes but don't print anything
+            if answer[-i:] == self.CHECK_STATUS[:i]:
+                return 0
+        # model is done generating [INST] -> saving answer
+        if answer[-len(self.CHECK_STATUS) :] == self.CHECK_STATUS:
+            return 1
+        # model can keep generating
+        return -1
+
+
+class OpenHermesChat(Chat):
+    """OpenHermes chat class
+
+    Args:
+        personality (str): OpenHermes personality (a description of what personality model has)
         examples (List[Dict[str, str]]): a list of examples of dialog [{"question": ..., "answer": ...}]
     """
     
@@ -58,7 +126,7 @@ class MistralChat(Chat):
     
     @property
     def prompt(self) -> str:
-        """Return Mistral prompt based on this structure
+        """Return OpenHermes prompt based on this structure
             <|im_start|>system
             {{ system_prompt }}
             <|im_start|>user
