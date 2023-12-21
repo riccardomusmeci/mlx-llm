d1a83f7ddd9900dfbfe4e404d1c21a6ac160d37e
Riccardo Musmeci <riccardomusmeci92@gmail.com>
2023-12-21 08:46:22 +0100

    fix chat generation

--- src/mlx_llm/chat/teknium.py
+++ src/mlx_llm/chat/teknium.py
@@ -27,9 +27,9 @@ class OpenHermesChat(Chat):
         """
         prompt = ""
         for example in self.examples:
-            prompt += self.USER + "\n" + example["user"] + self.END + "\n"
+            prompt += self.USER + "\n" + example["user"] + self.END + "\n" + self.ASSISTANT + "\n"
             if example["model"] is not None:
-                prompt += self.ASSISTANT + "\n" + example["model"] + self.END + "\n"
+                prompt += example["model"] + self.END + "\n"
         return prompt
     
     @property
@@ -46,3 +46,24 @@ class OpenHermesChat(Chat):
             str: prompt
         """
         return f"{self.SYSTEM}\n{self.personality}\n{self.history}"
+    
+    # def model_status(self, answer: str) -> int:
+    #     """Check if dialog is over
+
+    #     Returns:
+    #         int: -1 if model can keep generating, 0 if model is generating [INST], 1 if model is done generating [INST]
+    #     """
+    #     print(answer)
+    #     for i in range(len(self.END_STR) - 1, 0, -1):
+    #         # model is generating [INST] -> wait until it finishes but don't print anything
+    #         if answer[-i:] == self.END_STR[:i]:
+    #             return 0
+    #     for i in range(len(self.ASSISTANT) - 1, 0, -1):
+    #         # model is generating [INST] -> wait until it finishes but don't print anything
+    #         if answer[-i:] == self.ASSISTANT[:i]:
+    #             return 0
+    #     # model is done generating [INST] -> saving answer
+    #     if answer[-len(self.END_STR) :] == self.END_STR:
+    #         return 1
+    #     # model can keep generating
+    #     return -1
